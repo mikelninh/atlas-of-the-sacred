1e78928df72286421a3e78b2e73d6a79956d6255
Mikel <34214125+mikelninh@users.noreply.github.com>
2026-07-29 01:46:11 +0200

    fix: use Next links for header navigation

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -3,11 +3,9 @@ import Link from "next/link";
 export function Header() {
   return (
     <header className="site-header">
-      {/* The brand intentionally uses Next Link for internal navigation. */}
-      {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
       <Link className="brand" href="/"><span className="brand-mark">✦</span><span>Atlas of the Sacred</span></Link>
       <nav aria-label="Main navigation">
-        <a href="/#purpose">Purpose</a>
+        <Link href="/#purpose">Purpose</Link>
         <Link href="/journeys/common-thread/">Common Thread</Link>
         <Link href="/sites/giza/">Giza</Link>
         <Link href="/editorial/">Evidence</Link>
